b06ee2fc3a4154de6f37b5336c4d89d72f2f5c89
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2021-07-29 11:31:44 -0300

    Semana 03 - Aula 02

---
+++ semana_03/aula_02/exemplo1.py
@@ -0,0 +1,10 @@
+numero1 = int(input('Número 1: '))
+numero2 = int(input('Número 2: '))
+
+# numero1 = int(numero1)
+# numero2 = int(numero2)
+
+soma = numero1 + numero2
+
+print(soma, end='  ')
+print(soma)
